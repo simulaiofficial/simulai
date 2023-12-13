39e813c88c2718cc9ea4aa1c3f6a874a3571386e
wsieroci <w.sierocinski@gmail.com>
2023-12-13 02:49:24 +0100

    Improving API for serving example page

--- api/main.py
+++ api/main.py
@@ -3,7 +3,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from api.model import BlockText, BlockType, Details, BlockHeading, BlockDivider, BlockQuote, BlockOptions, BlockRadio, \
-    BlockInputTextAnswer, BlockInputEmailAnswer, BlockInputNumberAnswer, OptionItem, Page
+    BlockInputTextAnswer, BlockInputEmailAnswer, BlockInputNumberAnswer, OptionItem, Page, BlockCondition, \
+    ActionSelectedType
 
 app = FastAPI()
 
@@ -17,23 +18,35 @@ app.add_middleware(
 
 
 # Sample instances of each block type
-# block_text = BlockText(id='1', type=BlockType.Text, details=Details(value='Text Example'), isHidden=False)
-# block_heading = BlockHeading(id='2', type=BlockType.H1, details=Details(value='Heading Example'), isHidden=False)
-# block_divider = BlockDivider(id='3', type=BlockType.Divider, details=Details(), isHidden=False)
-# block_quote = BlockQuote(id='4', type=BlockType.Quote, details=Details(value='Quote Example'), isHidden=False)
+block_text = BlockText(id='1', type=BlockType.Text, details=Details(value='Text Example'), isHidden=False)
+block_heading = BlockHeading(id='2', type=BlockType.H1, details=Details(value='Heading Example'), isHidden=False)
+block_divider = BlockDivider(id='3', type=BlockType.Divider, details=Details(), isHidden=False)
+block_quote = BlockQuote(id='4', type=BlockType.Quote, details=Details(value='Quote Example'), isHidden=False)
 block_options = BlockOptions(id='5', type=BlockType.Options, details=Details(), isHidden=False, isRequired=True, setName=False, name='Options Block', items=[OptionItem(label='Option 1', isChecked=False), OptionItem(label='Option 2', isChecked=True)])
-# block_radio = BlockRadio(id='6', type=BlockType.Radio, details=Details(), isHidden=False, isRequired=False, setName=True, name='Radio Block', items=[OptionItem(label='Radio 1', isChecked=False), OptionItem(label='Radio 2', isChecked=True)])
-# block_input_text = BlockInputTextAnswer(id='7', type=BlockType.InputTextAnswer, details=Details(), isHidden=False, isRequired=True, setName=True, name='Text Input Block', minRequired=True, min=1, maxRequired=False, max=None)
-# block_input_email = BlockInputEmailAnswer(id='8', type=BlockType.InputEmailAnswer, details=Details(), isHidden=False, isRequired=False, setName=False, name='Email Input Block', isCompany=True)
-# block_input_number = BlockInputNumberAnswer(id='9', type=BlockType.InputNumberAnswer, details=Details(), isHidden=False, isRequired=True, setName=True, name='Number Input Block', minRequired=False, min=None, maxRequired=True, max=100)
+block_condition = BlockCondition(
+    id='10',
+    type=BlockType.Condition,
+    details=Details(),
+    isHidden=False,
+    whenBlockSelectedId='5',  # Assuming the condition depends on the BlockOptions with id '5'
+    isOperatorSelectedId='equals',  # This is a hypothetical operator ID, replace with actual if different
+    isOperatorValue='Option 2',  # The value to compare against, in this case, it could be a selection from BlockOptions
+    actionSelectedId=ActionSelectedType.Hide,  # This is a hypothetical action ID, replace with actual if different
+    actionBlockSelectedId='7'  # The id of the block to show/hide, in this case, BlockInputTextAnswer with id '7'
+)
+block_radio = BlockRadio(id='6', type=BlockType.Radio, details=Details(), isHidden=False, isRequired=False, setName=True, name='Radio Block', items=[OptionItem(label='Radio 1', isChecked=False), OptionItem(label='Radio 2', isChecked=True)])
+block_input_text = BlockInputTextAnswer(id='7', type=BlockType.InputTextAnswer, details=Details(), isHidden=False, isRequired=True, setName=True, name='Text Input Block', minRequired=True, min=1, maxRequired=False, max=None)
+block_input_email = BlockInputEmailAnswer(id='8', type=BlockType.InputEmailAnswer, details=Details(), isHidden=False, isRequired=False, setName=False, name='Email Input Block', isCompany=True)
+block_input_number = BlockInputNumberAnswer(id='9', type=BlockType.InputNumberAnswer, details=Details(), isHidden=False, isRequired=True, setName=True, name='Number Input Block', minRequired=False, min=None, maxRequired=True, max=100)
+
 
 # Creating the sample page with all block types
 sample_page = Page(
     name="🤖 simulai",
     blocks=[
-        # block_text, block_heading, block_divider, block_quote,
-        block_options,
-        # block_radio, block_input_text, block_input_email, block_input_number
+        block_text, block_heading, block_divider, block_quote,
+        block_options, block_condition,
+        block_radio, block_input_text, block_input_email, block_input_number
     ]
 )
 
--- api/model.py
+++ api/model.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from enum import Enum
 from typing import Optional, List, Union
 
+
 class BlockType(str, Enum):
     Text = 'TEXT'
     H1 = 'H1'
@@ -16,14 +17,17 @@ class BlockType(str, Enum):
     InputTextAnswer = 'INPUT_TEXT_ANSWER'
     InputNumberAnswer = 'INPUT_NUMBER_ANSWER'
 
+
 class ComparisonType(str, Enum):
     Text = 'TEXT'
     Number = 'NUMBER'
     Dropdown = 'DROPDOWN'
 
+
 class Details(BaseModel):
     value: Optional[str] = None
     blockTypes: Optional[List[BlockType]] = None
+
 
 class Block(BaseModel):
     id: str
@@ -31,27 +35,36 @@ class Block(BaseModel):
     details: Details
     isHidden: bool
 
+
 class BlockAnswer(Block):
     isRequired: bool
     setName: bool
     name: str
 
+class ActionSelectedType(str, Enum):
+    Jump = 'jump'
+    Hide = 'hide'
+
 class BlockCondition(Block):
     whenBlockSelectedId: str
     isOperatorSelectedId: str
     isOperatorValue: str
-    actionSelectedId: str
+    actionSelectedId: ActionSelectedType
     actionBlockSelectedId: str
+
 
 class OptionItem(BaseModel):
     label: str
     isChecked: bool
 
+
 class BlockOptions(BlockAnswer):
     items: List[OptionItem]
 
+
 class BlockRadio(BlockAnswer):
     items: List[OptionItem]
+
 
 class BlockInputTextAnswer(BlockAnswer):
     minRequired: bool
@@ -59,8 +72,10 @@ class BlockInputTextAnswer(BlockAnswer):
     maxRequired: bool
     max: Optional[int]
 
+
 class BlockInputEmailAnswer(BlockAnswer):
     isCompany: bool
+
 
 class BlockInputNumberAnswer(BlockAnswer):
     minRequired: bool
@@ -68,19 +83,26 @@ class BlockInputNumberAnswer(BlockAnswer):
     maxRequired: bool
     max: Optional[int]
 
+
 # Other Block types
 class BlockText(Block):
     pass
 
+
 class BlockHeading(Block):
     pass
+
 
 class BlockDivider(Block):
     pass
 
+
 class BlockQuote(Block):
     pass
 
+
 class Page(BaseModel):
     name: str
-    blocks: List[Block]
+    blocks: List[Union[
+        Block, BlockAnswer, BlockCondition, BlockOptions, BlockRadio, BlockInputTextAnswer,
+        BlockInputEmailAnswer, BlockInputNumberAnswer, BlockText, BlockHeading, BlockDivider, BlockQuote]]
