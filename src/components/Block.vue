a226ab86a8da89ac8c8b0b2e6f450e1d6200b9e2
wsieroci <w.sierocinski@gmail.com>
2023-12-16 03:56:42 +0100

    Showing next button only for chat

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -55,7 +55,7 @@
       <div>
         <button
             @click="goNextBlock()"
-            v-if="currentBlockNumber === blockNumber-1"
+            v-if="currentBlockNumber === blockNumber-1 && page.isChat"
             class="mt-2 mb-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300">
           Next
         </button>
@@ -81,7 +81,7 @@ const props = defineProps({
     },
   },
   page: {
-    type: Object as PropType<{ name: string, blocks: Block[] }>,
+    type: Object as PropType<{ name: string, isChat: boolean, blocks: Block[] }>,
     required: true,
   },
   blockTypes: {
