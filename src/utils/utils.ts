6132934213eb8a57f86f7862bdb5d1f0924937c2
wsieroci <w.sierocinski@gmail.com>
2023-12-29 10:09:28 +0100

    Fixing problem with displaying links inside blocks

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,6 +2,10 @@ import {Block, BlockAnswer, BlockComponents} from './types'
 
 export function markdownToHtml(mdString: string) {
     // Adapted from https://randyperkins2k.medium.com/writing-a-simple-markdown-parser-using-javascript-1f2e9449a558
+
+    // Regular expression to match URLs
+    const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/gi;
+
     return mdString
         .replace(/\\\*/g, '\\*\\*')
         // .replace(/_p/g, '<p>')
@@ -12,11 +16,16 @@ export function markdownToHtml(mdString: string) {
         // .replace(/(?<!\\)\*(?<!\\)\*(.*?)(?<!\\)\*(?<!\\)\*/g, '<strong>$1</strong>')
         // .replace(/(?<!\\)\*(.*?)(?<!\\)\*/g, '<em>$1</em>')
         // .replace(/\\\*/g, '*')
+        .replace(urlRegex, '<a href="$1" rel="nofollow">$1</a>') // Replace URLs with anchor tags
         .trim()
 }
 
 export function htmlToMarkdown(htmlString: string) {
+    // Regular expression to match HTML anchor tags
+    const anchorRegex = /<a href="([^"]+)"[^>]*>(.*?)<\/a>/gi;
+
     return htmlString
+        .replace(anchorRegex, '$1') // Convert anchor tags to Markdown link syntax
         .replaceAll('<p>', '')
         .replaceAll('</p>', '')
         .replaceAll('<strong>', '**')
