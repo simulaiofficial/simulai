dc2a39d667bf8b4c5d35bc5395887c05166ccb5e
wsieroci <w.sierocinski@gmail.com>
2024-02-04 16:46:04 +0100

    Add Skip button

--- src/components/Simulai.vue
+++ src/components/Simulai.vue
@@ -73,6 +73,14 @@
             />
           </div>
         </div>
+        <div
+            v-if="page.isChat && block.isRequired === false && i === currentVisibleBlock">
+          <button
+              @click="showNextBlock()"
+              class="mt-2 mb-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300 cursor-pointer">
+            Skip
+          </button>
+        </div>
       </div>
     </draggable>
     <div>
