30784681928dc3c1b8adc6f274a10b078aa71d91
wsieroci <w.sierocinski@gmail.com>
2024-02-25 10:50:14 +0100

    Fix tests

--- tests/unit/utils.test.ts
+++ tests/unit/utils.test.ts
@@ -6,6 +6,9 @@ test('isTextBlock should work for text blocks', () => {
   const knownTextBlocks = [
     BlockType.Text,
     BlockType.Quote,
+    BlockType.H1, 
+    BlockType.H2,
+    BlockType.H3
   ] as any[]
   Object.values(BlockType).forEach(type => {
     if (knownTextBlocks.includes(type as BlockType)) expect(isTextBlock(type)).toBeTruthy()
